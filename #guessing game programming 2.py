cb125b61ac5b87173be5147cb8a18c4dbb76f0c2
tadpole63 <tadpierski63@gmail.com>
2022-01-26 12:43:48 -0600

    apply functional code for game

---
+++ #guessing game programming 2.py
@@ -0,0 +1,27 @@
+#guessing game programming 2 
+import random
+number = random.randint(1, 101)
+
+tries = 0
+
+#welcome message
+print("\tWelcome to 'guess my number!'\t")
+trigger = str(input("type start to start"))
+while trigger == "start":
+    guess = int(input("im thinking of a number 1-100..."))
+
+    while guess != number:
+        if guess > number:
+            print("too high...")
+            guess = int(input("Guess again..."))
+            tries += 1
+        elif guess < number:
+            print("too low...")
+            guess = int(input("Guess again..."))
+            tries += 1
+
+    while guess == number:
+        print("You guessed it!!! yay!!")
+        print("it only took you ", tries, "tries!")
+        break
+    trigger = str(input("Type start to go again and end to end."))
