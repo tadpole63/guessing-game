b5a9eccd0551853ba921edef691850a1d56b3937
tadpole63 <tadpierski63@gmail.com>
2022-01-27 07:28:32 -0600

    update 1/27/22
    Fixed formatting problems
    Added spaces on user entrance feilds
    added parting message

--- #guessing game programming 2.py
+++ #guessing game programming 2.py
@@ -6,22 +6,25 @@ tries = 0
 
 #welcome message
 print("\tWelcome to 'guess my number!'\t")
-trigger = str(input("type start to start"))
+trigger = str(input("type start to start "))
 while trigger == "start":
-    guess = int(input("im thinking of a number 1-100..."))
+    guess = int(input("im thinking of a number 1-100... "))
 
     while guess != number:
         if guess > number:
             print("too high...")
-            guess = int(input("Guess again..."))
+            guess = int(input("Guess again... "))
             tries += 1
         elif guess < number:
             print("too low...")
-            guess = int(input("Guess again..."))
+            guess = int(input("Guess again... "))
             tries += 1
 
     while guess == number:
         print("You guessed it!!! yay!!")
         print("it only took you ", tries, "tries!")
         break
-    trigger = str(input("Type start to go again and end to end."))
+    trigger = str(input("Type start to go again and end to end. "))
+
+
+print("Thanks for playing!")
